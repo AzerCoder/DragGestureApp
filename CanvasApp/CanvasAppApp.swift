6d375083361997b566ae260622b91737470d2a76
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-09-17 14:49:26 +0500

    Initial Commit

---
+++ CanvasApp/CanvasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasAppApp.swift
+//  CanvasApp
+//
+//  Created by A'zamjon Abdumuxtorov on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CanvasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
